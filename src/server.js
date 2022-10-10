3a87c1519904378fed5312be8c69c99d4113bc7f
Sai Krishna <97687357+saikmusunuri@users.noreply.github.com>
2022-10-10 01:15:50 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,10 +10,54 @@ const app = express();
 // world wide web).
 const port = process.env.PORT || 3000;
 
-
+const cookieParser = require('cookie-parser');
 // The main page of our website
+// The main page of our website
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//});
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+/*const {encode} = require('html-entities');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+const name = req.query.name || "World";
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>An Example Title</title>
+        <link rel="stylesheet" href="app.css">
+      </head>
+      <body>
+         <h1>Hello, ${encode(name)}!</h1>
+        <p>HTML is so much better than a plain string!</p>
+      </body>
+    </html>
+  `);
+});
+*/
+
+app.use(cookieParser());
+let nextVisitorId = 1;
+var currentTime = new Date();
+app.get('/', (req, res) => {
+  if(req.cookies['visitorId']){
+  res.cookie('visitorId', nextVisitorId);}
+  else
+  res.cookie('visitorId', nextVisitorId++);
+  res.cookie('visited', Date.now().toString());
+  res.render('welcome', {
+    name: req.query.name || "World",
+    datetime: req.query.date || new Date().toLocaleString(),
+    visitor_count: req.query.nextVisitorId || nextVisitorId,
+    visit_time: req.query.visited || Math.round((new Date().getTime() - currentTime.getTime()) / 1000),
+  });
+  currentTime = new Date();
+  
+console.log(req.cookies);
 });
 
 // Start listening for network connections
