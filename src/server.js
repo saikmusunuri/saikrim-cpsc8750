fcaface848fb804e2a837db03104b817b932658b
Sai Krishna <97687357+saikmusunuri@users.noreply.github.com>
2022-10-30 21:43:33 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // create a new server application
 const app = express();
+const fetch = require('node-fetch');
 
 // Define the port we will listen on
 // (it will attempt to read an environment global
@@ -67,6 +68,47 @@ app.get('/', (req, res) => {
 
   
 console.log(req.cookies);
+});
+
+app.get("/trivia", async(req, res) => {
+    // fetch the data
+    const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+
+    // fail if bad response
+    if (!response.ok) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+        return;
+    }
+
+    // interpret the body as json
+    const content = await response.json();
+
+    // fail if db failed
+    if (content.response_code !== 0) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+        return;
+    }
+
+    // respond to the browser
+    correctAnswer = content.results[0]['correct_answer']
+    answers = content.results[0]['incorrect_answers']
+    answers.push(correctAnswer)
+    let tempans = answers.sort(function() {
+        return Math.random() - 0.5;
+    });
+    const answerLinks = tempans.map(answer => {
+        return `<a style='color:white' href="javascript:alert('${
+          answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+          }')">${answer}</a>`
+    })
+    res.render('trivia', {
+        question: content.results[0]['question'],
+        category: content.results[0]['category'],
+        difficulty: content.results[0]['difficulty'],
+        answers: answerLinks
+    })
 });
 
 // Start listening for network connections
